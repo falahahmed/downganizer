cd048b70e73cb02639e9a4d8d234b483726e91df
falahahmed <kpfalah99@gmail.com>
2025-06-18 15:20:18 +0530

    Added functionality to check status

--- lib/services.sh
+++ lib/services.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[1;37m'
+
 dg_start () {
     $SCRIPT_DIR/lib/monitor.sh & disown
     echo "Watches established"
@@ -62,18 +66,47 @@ dg_disable () {
     echo "Downganzier disabled"
 }
 
-MAIN_PID=0
-SUB_PID=0
+is_array() {
+  local var_name="$1"
+  if ( unset "${var_name}[0]" 2>/dev/null ); then
+    return 0
+  else
+    return 1
+  fi
+}
 
 status () {
-    date
-    echo "Hello "
-    var=$(pgrep -f "$SCRIPT_DIR/lib/monitor.sh")
-    echo "var: $var"
+    title="$WHITE Downganizer Status"
+    load="$RED Autostart not loaded$WHITE"
+    status="$RED Stopped $WHITE"
+    cpu=0
+    mem=0
+
+    inot=$(pgrep -f inotifywait | head -n 1)
+    inotpid=0
+    moni=$(pgrep -f monitor.sh | head -n 1)
+
+    if [[ -n "$inot" ]] && [[ -n "$moni" ]]; then
+        start=$(ps -p "$inot" -o stime=)
+        status="$GREEN Active $WHITE(Since: $start)"
+        cpu=$(ps -p $inot -o %cpu | grep -v "CPU" | cut -d ' ' -f2)
+        mem=$(ps -p $inot -o %mem | grep -v "MEM" | cut -d ' ' -f2)
+    fi
+
+    if [[ -f /home/$USER/.config/autostart/downganizer.desktop ]]; then
+        load="Loaded (/home/$USER/.config/autostart/downganizer.desktop)"
+    fi
+    echo -e "$title
+    Loaded: $load
+    Status: $status
+    CPU Usage: $cpu%
+    Memory Usage: $mem%
+    Press 'q' to exit"
 }
 
 dg_status() {
-    watch -n 1 -t "bash -c '$(declare -f status); status'"
+    status
+    # watch -n 1 -t "bash -c '$(declare -f status); status'"
 }
 
 create_autostart_file () {
